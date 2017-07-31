af210bcbcd97fad1b14bcdd80936b5d3c3e11008
Jason Bowden <jason.bowden1000@gmail.com>
2017-07-31 16:12:47 -0500

    Improved readability of math spec

--- test/spec/math-spec.js
+++ test/spec/math-spec.js
@@ -1,21 +1,21 @@
 const expect = require('chai').expect;
 const math = require('./../../problems/math');
 
-let reverseDigitContext = (f) => {
-  describe(f.name, () => {
+let reverseDigitContext = (reverseDigits) => {
+  describe(reverseDigits.name, () => {
     it('should return a number in reverse order', () => {
       let num = 4242;
-      expect(f(num)).to.equal(2424);
+      expect(reverseDigits(num)).to.equal(2424);
     });
 
     it('should preserve the sign of reversed digits', () => {
       let num = -314;
-      expect(f(num)).to.equal(-413);
+      expect(reverseDigits(num)).to.equal(-413);
     });
 
     it('should not return octals for numbers that end in zero', () => {
       let num = 2220;
-      expect(f(num)).to.equal(222);
+      expect(reverseDigits(num)).to.equal(222);
     });
   });
 };
