49d6c725f803329b900d8841d79e798d66d2c102
Jason Bowden <jason.bowden1000@gmail.com>
2017-08-02 09:43:53 -0500

    Began adding Link spec

---
+++ test/spec/classes/Link-spec.js
@@ -0,0 +1,10 @@
+let Link = require('./../../../classes/Link');
+let expect = require('chai').expect;
+
+describe('Link', () => {
+  describe('when updating', () => {
+    it('should only allow links as successors', () => {
+
+    });
+  });
+});
