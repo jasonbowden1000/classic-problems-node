d97b01723f854f9c908dc1aefbf1d847f613ba05
Jason Bowden <jason.bowden1000@gmail.com>
2018-08-12 19:14:31 -0500

    Refined math spec

--- test/spec/problems/math-spec.js
+++ test/spec/problems/math-spec.js
@@ -3,17 +3,14 @@ const math = require('./../../../problems/math');
 
 describe('Reverse digits', () => {
   it('should return a number in reverse order', () => {
-    let num = 4242;
-    expect(math.reverseDigits(num)).to.equal(2424);
+    expect(math.reverseDigits(4242)).to.equal(2424);
   });
 
   it('should preserve the sign of reversed digits', () => {
-    let num = -314;
-    expect(math.reverseDigits(num)).to.equal(-413);
+    expect(math.reverseDigits(-314)).to.equal(-413);
   });
 
   it('should not return octals for numbers that end in zero', () => {
-    let num = 2220;
-    expect(math.reverseDigits(num)).to.equal(222);
+    expect(math.reverseDigits(2220)).to.equal(222);
   });
 });
