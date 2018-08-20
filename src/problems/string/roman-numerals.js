f93ec666ef0e3b7ad68722c80e78c0abd86f85f6
Jason Bowden <jason.bowden1000@gmail.com>
2018-08-19 20:04:09 -0500

    Improved nomenclature

--- src/problems/string/roman-numerals.js
+++ src/problems/string/roman-numerals.js
@@ -1,31 +1,32 @@
-const numberMap = new Map([
+const romanMap = new Map([
   ["I", 1], ["IV", 4], ["V", 5], ["IX", 9], ["X", 10], ["XL", 40], ["L", 50],
   ["XC", 90], ["C", 100], ["CD", 400], ["D", 500], ["CM", 900], ["M", 1000]
 ]);
 
-function isRoman(romanNum) {
-  return /^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/.test(romanNum);
+function isRoman(n) {
+  return /^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/.test(n);
 }
 
 function arabicToRoman(num) {
-  let romanNum = '';
-  let divisorMap = Array.from(numberMap).map(([k, v]) => {
-    return { arabic: v, roman: k }
-  }).reverse();
+  let roman = '';
+  let pairs = Array
+    .from(romanMap)
+    .map(([k, v]) => ({ arabic: v, roman: k }))
+    .reverse();
 
-  for (let obj of divisorMap) {
+  for (let obj of pairs) {
     while (num >= obj.arabic) {
-      romanNum = romanNum + obj.roman;
+      roman = roman + obj.roman;
       num -= obj.arabic;
     }
   }
 
-  return romanNum;
+  return roman;
 }
 
 function romanToArabic(romanNum) {
   return [...romanNum]
-    .map(l => numberMap.get(l))
+    .map(l => romanMap.get(l))
     .reduceRight((acc, curr, i, arr) => {
       return curr >= arr[i + 1] ? acc + curr : acc - curr;
     });
