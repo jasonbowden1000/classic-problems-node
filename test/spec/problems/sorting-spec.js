3c03f411e22692b39cdb78223ac8c4a9e58609b3
Jason Bowden <jason.bowden1000@gmail.com>
2018-09-11 14:10:33 -0500

    basic sorted merge

--- test/spec/problems/sorting-spec.js
+++ test/spec/problems/sorting-spec.js
@@ -12,8 +12,12 @@ describe('Sorter', () => {
       expect(sorter.merge(sortedArray1, sortedArray2)).to.not.eql([1, 2, 3, 4, 5, 6, 7]);
     });
 
-    xit('should return an ordered array when given duplicate elements', () => {
-
+    it('should return an ordered array when given duplicate elements', () => {
+      let sortedArray1 = [5, 5, 6, 13, 13, 13, 17];
+      let sortedArray2= [3, 6, 7, 11, 19, 19];
+      let mergedArray = [3, 5, 5, 6, 6, 7, 11, 13, 13, 13, 17, 19, 19];
+      expect(sorter.merge(sortedArray1, sortedArray2)).to.eql(mergedArray);
+      expect(sorter.merge(sortedArray1, sortedArray2)).to.not.eql([3, 5, 6, 7, 11, 13, 17, 19]);
     });
   });
 });
